082a5c1f5145ee03e01c762da42d898c1bc8228d
Jakub Lecki <lec.jakub@gmail.com>
2022-03-15 23:49:33 +0100

    trip_service: Create a trip service for receiving bocker messages.
    The service is constantly waiting for messages and prints them on screen.
    
    Signed-off-by: Jakub Lecki <lec.jakub@gmail.com>

---
+++ trip_service/trip_service.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import pika, os, sys, time
+
+def main():
+    print("TRIP SERIVCE STARTED")
+    time.sleep(5)
+    credentials = pika.PlainCredentials('guest', 'guest')
+    parameters = pika.ConnectionParameters('hipotap_brocker',
+                                            5672,
+                                            '/',
+                                            credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+    channel.basic_consume(queue='hello',
+            auto_ack=True,
+            on_message_callback=callback)
+
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
