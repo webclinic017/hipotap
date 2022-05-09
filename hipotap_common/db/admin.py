4553439d32da7f08a048b3626539b3258eb41044
KatsiarynaYab <37000261+KatsiarynaYab@users.noreply.github.com>
2022-05-09 22:24:18 +0200

    Add scrapper

--- hipotap_common/db/admin.py
+++ hipotap_common/db/admin.py
@@ -1,36 +1,28 @@
 from datetime import datetime
 
 from . import DBModel, engine, db_session, Customer_Table, Offer_Table
+from .scrapper import Scrapper
+
 
 def init_db():
+    scrapper = Scrapper()
+    offers_dict = scrapper.parce()
     DBModel.metadata.drop_all(engine)
     DBModel.metadata.create_all(engine)
 
     db_session.add(Customer_Table(email="a@a", name="Joe", surname="Doe", password="123"))
-
-    db_session.add(Offer_Table(
-        title='Super wypoczynek w Costa Brava',
-        description='super wypoczynek dla całej rodziny',
-        place='Costa Brava',
-        hotel='Grand Hotel',
-        max_adult_count=2,
-        max_children_count=2,
-        price_adult=2.20,
-        price_children=3.30,
-        date_start=datetime.strptime('23/02/23 00:00:00', '%d/%m/%y %H:%M:%S'),
-        date_end=datetime.strptime('01/03/23 00:00:00', '%d/%m/%y %H:%M:%S')
-    ))
-    db_session.add(Offer_Table(
-        title='Super wypoczynek w Grecji',
-        description='super wypoczynek dla całej dorosłych',
-        place='Grecja',
-        hotel='Hotel Mercury',
-        max_adult_count=2,
-        max_children_count=0,
-        price_adult=2.20,
-        price_children=0,
-        date_start=datetime.strptime('11/09/23 00:00:00', '%d/%m/%y %H:%M:%S'),
-        date_end=datetime.strptime('22/09/23 00:00:00', '%d/%m/%y %H:%M:%S')
-    ))
+    for offer in offers_dict:
+        db_session.add(Offer_Table(
+            title=offer["title"],
+            description=offer["description"],
+            place=offer["place"],
+            hotel=offer["hotel"],
+            max_adult_count=offer["max_adult_count"],
+            max_children_count=offer["max_children_count"],
+            price_adult=offer["price_adult"],
+            price_children=offer["price_children"],
+            date_start=datetime.strptime(offer["date_start"], '%d/%m/%y %H:%M:%S'),
+            date_end=datetime.strptime(offer["date_end"], '%d/%m/%y %H:%M:%S')
+        ))
 
     db_session.commit()
